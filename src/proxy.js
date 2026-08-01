73a7b897623af5c211363f0e4185ae768596aefa
Aji Noto Sutrisno <ajinotosutrisno212@gmail.com>
2026-08-01 21:27:40 +0700

    fix: update csp in vercel json and proxy middleware

--- src/proxy.js
+++ src/proxy.js
@@ -10,9 +10,9 @@ export function proxy(request) {
       "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: blob:",
+      "img-src 'self' data: blob: https://*.nasa.gov https://apod.nasa.gov https://images-assets.nasa.gov https://*.wikimedia.org https://upload.wikimedia.org",
       "worker-src 'self' blob:",
-      "connect-src 'self'",
+      "connect-src 'self' https://api.nasa.gov https://*.nasa.gov",
     ].join('; ')
   );
 
